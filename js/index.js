270ebb93d5e3b74b370724d8638e26b8f78b1d71
Oleksandr <yavorschukoleksandr@gmail.com>
2022-02-20 12:02:54 +0200

    fixed

--- js/index.js
+++ js/index.js
@@ -9,14 +9,15 @@ console.log(sum);
 console.log(Math.floor(breadprice));
 console.log(Math.floor(beerprice));
 console.log(Math.floor(meatprice));
-console.log(Math.floor((Math.floor(meatprice))+(Math.floor(beerprice))+(Math.floor(breadprice))));
+console.log(Math.floor(sum));
 console.log(Math.round((sum)/100)*100);
-console.log (!(Math.floor((Math.floor(meatprice))+(Math.floor(beerprice))+(Math.floor(breadprice)))%2));
+console.log (!(Math.floor()%2));
 console.log(500-sum);
 console.log(+(sum/3).toFixed(2));
-console.log (Math.round(Math.random() * (99 - 1) + 1))
-console.log(sum-(Math.round(Math.random() * (99 - 1) + 1)))
-console.log(Math.round((sum/2)-(sum-(Math.round(Math.random() * (99 - 1) + 1)))))
+const discount = Math.round(Math.random() * (99 - 1) + 1);
+console.log(discount);
+console.log(sum-discount);
+console.log(Math.round((sum/2)-(discount)));
 
 document.writeln(
     `
@@ -26,11 +27,11 @@ document.writeln(
             <b>Вартість всіх товарів:</b> ${sum}<br>
             <b>Вартість всіх товарів (спершу округленна):</b> ${Math.floor(sum)}<br>
             <b>Вартість всіх товарів (округленна до сотень):</b> ${Math.round((sum)/100)*100}<br>
-            <b>Чи є сума парним числом:</b> ${!(Math.floor(((Math.floor(meatprice))+(Math.floor(beerprice))+(Math.floor(breadprice))))%2)}<br>
+            <b>Чи є сума парним числом:</b> ${!(Math.floor(sum)%2)}<br>
             <b>Сума решти, при оплаті 500грн:</b> ${500-sum}<br>
             <b>Середнє значення цін:</b> ${+(sum/3).toFixed(2)}</br>
-            <b>Випадкова знижка:</b> ${Math.round(Math.random() * (99 - 1) + 1)}<br>
-            <b>Сума до оплати, після знижки:</b> ${sum-(Math.round(Math.random() * (99 - 1) + 1))}<br>
-            <b>Чистий прибуток, після знижки:</b> ${Math.round((sum/2)-(sum-(Math.round(Math.random() * (99 - 1) + 1))))}<br>
+            <b>Випадкова знижка:</b> ${discount}<br>
+            <b>Сума до оплати, після знижки:</b> ${sum-discount}<br>
+            <b>Чистий прибуток, після знижки:</b> ${Math.round((sum/2)-(sum-discount))}<br>
         </i>
     `)
